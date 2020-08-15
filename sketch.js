852d22baaa0a68b1015a254d0a89004a23ef1395
tharni20 <68662023+tharni20@users.noreply.github.com>
2020-08-15 12:22:29 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
   wall=createSprite(1500,200,60,height/2);
 
   speed=random(55,90);
-  weight=random(400,150);
+  weight=random(400,1500);
 
   car.velocityX = speed;
 }
@@ -26,18 +26,21 @@ if(wall.x-car.x < (car.width+wall.width)/2)
 {
    car.velocityX = 0;
 
+
   var deformation=0.5 * weight * speed * speed/22509;
+  textSize(20);
+  text("deformation"+deformation,1200,100);
   if(deformation>180)
   {
-    car.shapeColor=color(255,0,0);
+    car.shapeColor= "red";
   }
   if(deformation<180 && deformation>100)
   {
-    car.shapeColor=color(230,230,0);
+    car.shapeColor= "yellow";
   }
   if(deformation<100)
   {
-    car.shapeColor= (0,255,0);
+    car.shapeColor= "green";
     
   }
 }
